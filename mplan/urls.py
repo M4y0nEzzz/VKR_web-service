53c56fedafe58cb8f7f31bd679a9e626da9436ef
Михаил Коничев <konichev.04@mail.ru>
2025-11-20 13:36:13 +0300

    добавление LDAP-заглушки

--- mplan/urls.py
+++ mplan/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.ldap_login, name='ldap_login'),
+    path('', views.home, name='home'),
 ]
 
 if settings.DEBUG:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+# ВНИМАНИЕ !!!
+# Авторизация через LDAP реализована через заглушку.
+# Используется псевдо-авторизация через стандартную форму Django.
+# Выводимое сообщение об успешной авторизации не имеет смысла.
+def ldap_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Вы успешно вошли. Данные синхронизированы с LDAP.")
+            return redirect('home')
+        else:
+            messages.error(request, "Неверное имя пользователя или пароль.")
+    return render(request, "login.html")
